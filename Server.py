238c04fcc9f667a5626ef77be54d8912380931f9
HeoTeaRyang <cake0702@naver.com>
2024-06-03 23:01:50 +0900

    db 구성파일 pickle로 변경에 따라 파일 입출력 코드 변경

--- Server.py
+++ Server.py
@@ -1,69 +1,40 @@
+import pickle
 import My_Structs
 
-shop_owners = []
-shop_staffs = []
-customers = []
-shops = []
+with open("db\members\shop_owners.pickle","rb") as fr:
+    shop_owners = pickle.load(fr)
 
-file = open(r"db\shop_owners.txt", "r", encoding="UTF-8")
-line = file.readline()
-while line:
-    tmp_l = line.split(',')
-    tmp_shop_nums = []
-    for i in range(5,len(tmp_l)):
-        tmp_shop_nums.append(int(tmp_l[i]))
-    tmp = My_Structs.ShopOwner(tmp_l[0], tmp_l[1], tmp_l[2], tmp_l[3], int(tmp_l[4]), tmp_shop_nums)
-    shop_owners.append(tmp)
-    line = file.readline()
-file.close()
+with open("db\members\shop_staffs.pickle","rb") as fr:
+    shop_staffs = pickle.load(fr)
 
-file = open(r"db\shop_staffs.txt", "r", encoding="UTF-8")
-line = file.readline()
-while line:
-    tmp_l = line.split(',')
-    tmp = My_Structs.ShopStaff(tmp_l[0], tmp_l[1], tmp_l[2], tmp_l[3], int(tmp_l[4]), int(tmp_l[5]))
-    shop_staffs.append(tmp)
-    line = file.readline()
-file.close()
+with open("db\members\customers.pickle","rb") as fr:
+    customers = pickle.load(fr)
 
-file = open(r"db\customers.txt", "r", encoding="UTF-8")
-line = file.readline()
-while line:
-    tmp_l = line.split(',')
-    tmp = My_Structs.Member(tmp_l[0], tmp_l[1], tmp_l[2], tmp_l[3], int(tmp_l[4]))
-    customers.append(tmp)
-    line = file.readline()
-file.close()
-
-file = open(r"db\shops.txt", "r", encoding="UTF-8")
-line = file.readline()
-while line:
-    tmp_l = line.split(',')
-    tmp_operation_hours = list(map(int, tmp_l[3:-1]))
-    tmp = My_Structs.Shop(tmp_l[0],tmp_l[1],tmp_l[2],tmp_operation_hours,int(tmp_l[-1]))
-    shops.append(tmp)
-    line = file.readline()
-file.close()
+with open("db\shops.pickle","rb") as fr:
+    shops = pickle.load(fr)
 
 def addShopOwner(shop_owner):
     shop_owners.append(shop_owner)
-    tmp = [shop_owner.getID(),shop_owner.getPW(),shop_owner.getName(),shop_owner.getTel(),str(shop_owner.getNum())]+list(map(str,shop_owner.getShopNums()))
-    file = open('db\shop_owners.txt', 'a', encoding="UTF-8")
-    file.writelines(','.join(tmp))
-    file.write("\n")
-    file.close()
+    with open("db\members\shop_owners.pickle","wb") as fw:
+        pickle.dump(shop_owners,fw)
+        
 def addShopStaff(shop_staff):
     shop_staffs.append(shop_staff)
-    tmp = [shop_staff.getID(),shop_staff.getPW(),shop_staff.getName(),shop_staff.getTel(),str(shop_staff.getNum()),str(shop_staff.getShopNum())]
-    file = open('db\shop_staffs.txt', 'a', encoding="UTF-8")
-    file.writelines(','.join(tmp))
-    file.write("\n")
-    file.close()
+    with open("db\members\shop_staffs.pickle","wb") as fw:
+        pickle.dump(shop_staffs,fw)
+    
 def addCustomer(customer):
     customers.append(customer)
-    tmp = [customer.getID(),customer.getPW(),customer.getName(),customer.getTel(),str(customer.getNum())]
-    file = open('db\customers.txt', 'a', encoding="UTF-8")
-    file.writelines(','.join(tmp))
-    file.write("\n")
-    file.close()
+    with open("db\members\customers.pickle","wb") as fw:
+        pickle.dump(customers,fw)
+    
 # def addShop(shop):
+# def delShop():
+# def addStaffToShop():
+# def addProcedureToStaff():
+# def addReservation():
+# def addSchedule():
+# def delReservation():
+# def delSchedule():
+# def getReservations():
+# def getSchedules():
